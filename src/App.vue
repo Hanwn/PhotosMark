8878f5d0afb88bbe8c5738f33c43b44c6d2d2985
hanwn <hanwn7721@gmail.com>
2023-09-16 23:23:35 +0800

    wip: code photo container

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Banner from "@/components/Banner.vue";
+import Content from "@/components/Content.vue";
+</script>
+
+<template>
+  <Content></Content>
+  <Banner></Banner>
+</template>
+
+<style scoped>
+</style>
