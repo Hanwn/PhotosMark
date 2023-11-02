22119956b4996fab11bde5544c09a9a5f585d075
hanwn <hanwn7721@gmail.com>
2023-11-03 00:42:42 +0800

    feat: support reset parameter

--- src/components/ParameterArea.vue
+++ src/components/ParameterArea.vue
@@ -3,13 +3,22 @@
 import {defineRender} from "@/store/defineRender";
 import {getImageData} from "@/utils/readFile";
 import {getExifData} from "@/utils/readExif";
-import {defineIcon, pushToIconCache, pushToImgList} from "@/store/defineImg";
+import {
+  defineExifCache,
+  defineIcon,
+  id2Src,
+  pushToExifCache,
+  pushToIconCache,
+  pushToID2Src,
+  pushToImgList
+} from "@/store/defineImg";
 import {loadImg} from "@/utils/loadImg";
 import {getIconSrc} from "@/utils/getIcon";
 import {genRenderItem, getMarkInfo} from "@/utils/parameterInfoConfig";
 import {defineFactor} from "@/store/defineFactor";
 
-const {renderIdx, renderList, pushToRenderList}= defineRender()
+const {renderIdx, renderList, pushToRenderList, setRenderIdx, setRenderListByRenderIdx}= defineRender()
+const {exifCache} = defineExifCache()
 const {iconCache} = defineIcon()
 const {factor} = defineFactor()
 
@@ -26,6 +35,7 @@ const readImgData = async function(file) {
   exifData.LEN = exifData.LEN.replace(/\u0000/g, "")
   let src = URL.createObjectURL(file)
   pushToImgList(src)
+  pushToExifCache(src, exifData)
 
   const img = await loadImg(src)
   const iconName = getIconSrc(exifData)
@@ -45,6 +55,8 @@ const readImgData = async function(file) {
   const genMarkInfo = getMarkInfo(exifData, padding, middle, rectW, rectH, img.height, iconImg)
   const renderItem = genRenderItem(img, genMarkInfo)
   pushToRenderList(renderItem)
+  setRenderIdx(renderIdx.value + 1)
+  pushToID2Src(renderIdx.value, src)
 }
 
 function uploadFiles(e) {
@@ -58,12 +70,45 @@ function uploadFiles(e) {
   }
 }
 
+async function reset() {
+  console.log(renderIdx.value, id2Src.get(renderIdx.value))
+  if (id2Src.has(renderIdx.value)) {
+    const src = id2Src.get(renderIdx.value)
+    const exifData = exifCache.get(src)
+
+    const img = await loadImg(src)
+    const iconName = getIconSrc(exifData)
+    const iconSrc = `https://pic-1301492519.cos.ap-shanghai.myqcloud.com/icon/${iconName}`
+    let iconImg = ""
+    if (iconCache.has(iconSrc)) {
+      iconImg = iconCache.get(iconSrc)
+    } else {
+      iconImg = await loadImg(iconSrc)
+      pushToIconCache(src)
+    }
+
+    const padding = 100
+    const rectH = img.height * factor.value
+    const middle = img.height + rectH/3
+    const rectW = img.width
+    const genMarkInfo = getMarkInfo(exifData, padding, middle, rectW, rectH, img.height, iconImg)
+    const renderItem = genRenderItem(img, genMarkInfo)
+    setRenderListByRenderIdx(renderIdx.value, renderItem)
+
+  }
+  // renderList[renderIdx.value] = genRenderItem()
+}
+
 </script>
 
 <template>
   <div>
-    <input type="file" id="upload" multiple="multiple" @change="uploadFiles">
+    <input type="file" id="upload" multiple="multiple" @change="uploadFiles"/>
+    <input type="button" value="reset" @click="reset">
     <input v-model="renderList[renderIdx].deviceInfoConfig.text"/>
+    <input v-model="renderList[renderIdx].lensInfoConfig.text"/>
+    <input v-model="renderList[renderIdx].timeInfoConfig.text"/>
+    <input v-model="renderList[renderIdx].parameterInfoConfig.text"/>
   </div>
 
 </template>
--- src/store/defineImg.js
+++ src/store/defineImg.js
@@ -3,9 +3,19 @@ import {defineRender} from "@/store/defineRender";
 
 const imgSrcList = reactive([])
 const iconCache = reactive(new Map())
+const exifCache = reactive(new Map())
+const id2Src = reactive(new Map())
 
 function pushToImgList(src) {
     imgSrcList.push(src)
+}
+
+function pushToID2Src(id, src) {
+    id2Src.set(id, src)
+}
+
+function pushToExifCache(src, exifData) {
+    exifCache.set(src, exifData)
 }
 
 function pushToIconCache(src, iconImg) {
@@ -31,4 +41,16 @@ function defineIcon() {
     }
 }
 
-export {defineImgList, defineIcon, pushToImgList, pushToIconCache}
+function defineExifCache() {
+    return {
+        exifCache
+    }
+}
+
+function defineId2Src() {
+    return {
+        id2Src
+    }
+}
+
+export {defineImgList, defineIcon, defineExifCache, id2Src, pushToImgList, pushToIconCache, pushToExifCache, pushToID2Src}
