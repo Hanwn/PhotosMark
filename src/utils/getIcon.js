b52037bb0a52e77ca630ad8374cab787912aa5ad
hanwn <hanwn7721@gmail.com>
2023-11-04 23:00:58 +0800

    chore: add some device icon

--- src/utils/getIcon.js
+++ src/utils/getIcon.js
@@ -4,26 +4,30 @@ const getIconSrc = function (exifData) {
     const model = exifData.Model
     const make = exifData.Make
 
+    console.log(model,make)
+
     const deviceModel = model.toLowerCase()
     const deviceMake = make.toLowerCase()
-    let iconSrc = ""
+    let iconSrc = "Nikon100.svg"
+    const deviceMap = new Map([
+        ["nikon", "Nikon2.svg"],
+        ["sony", "Sony.svg"],
+        ["canon", "Canon.svg"],
+        ["fujifilm", "Fujifilm.svg"],
+        ["dij", "Dji.svg"],
+        ["hasselblad", "hasselblad.svg"],
+        ["lumix", "lumix.svg"],
+        ["gopro", "gopro.svg"],
+        ["apple", "apple.svg"],
+        ["pentax", "pentax.svg"],
+        ["ricoh", "ricoh.svg"]
+    ])
 
-    if (deviceModel.includes("nikon") || deviceMake.includes("nikon")) {
-        iconSrc = "Nikon2.svg"
-    } else if (deviceModel.includes("sony") || deviceMake.includes("sony")) {
-        iconSrc = "Sony.svg"
-    } else if (deviceModel.includes("canon") || deviceMake.includes("canon")) {
-        iconSrc = "Canon.svg"
-    }else if (deviceModel.includes("fujifilm") || deviceMake.includes("fujifilm")) {
-        iconSrc = "Fujifilm.svg"
-    } else if (deviceModel.includes("dji") || deviceMake.includes("dji")) {
-        iconSrc = "Dji.svg"
-    }else if (deviceModel.includes("hasselblad") || deviceMake.includes("hasseldblad")) {
-        iconSrc = "Hasselblad.svg"
-    } else if (deviceModel.includes("lumix") || deviceMake.includes("lumix")) {
-        iconSrc = "Lumix.svg"
-    } else {
-        iconSrc = "Sony.svg"
+    if (deviceMap.has(deviceModel)) {
+        return deviceMap.get(deviceModel)
+    }
+    if (deviceMake.hash(deviceMake)) {
+        return deviceMap.get(deviceMake)
     }
 
     return iconSrc
