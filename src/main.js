6c0ed551afb57b889daa65a5822a98e5ef90c68d
hanwn <hanwn7721@gmail.com>
2023-11-05 16:47:13 +0800

    perf: optimize module size

--- src/main.js
+++ src/main.js
@@ -3,16 +3,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueKonva from 'vue-konva';
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-}
 
 app.use(VueKonva)
-app.use(ElementPlus)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// vite.config.ts
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
   }
 })
