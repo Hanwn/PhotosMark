9ccea761ed7904e794a11139c07aad20deaf7dc8
hanwn <hanwn7721@gmail.com>
2023-11-11 16:01:50 +0800

    :recycle: remove factor form global

--- src/themes/mi/mi.js
+++ src/themes/mi/mi.js
@@ -2,14 +2,10 @@ import {genRenderItem, getMarkInfo} from "@/themes/mi/parameterInfoConfig";
 import {allCanvasConfigMap} from "@/store/defineCanvasConfig";
 import {ref} from 'vue'
 
-const padding = 100
-const factor = ref(0.1)
+// const factor = ref(0.1)
 
 function PreviewRenderMi(uid, img, exifData, iconImg) {
-    const rectH = img.height * factor.value
-    const rectW = img.width
-    const middle = img.height + rectH / 3
-    const genMarkInfo = getMarkInfo(exifData, padding, middle, rectW, rectH, img.height, iconImg)
+    const genMarkInfo = getMarkInfo(exifData, img, iconImg)
     const renderItem = genRenderItem(img, genMarkInfo)
     allCanvasConfigMap.set(uid, renderItem)
 }
