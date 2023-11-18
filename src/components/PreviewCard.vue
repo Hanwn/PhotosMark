c661fb2d995c545604b41a6f48a4d285acac44a9
hanwn <hanwn7721@gmail.com>
2023-11-18 11:57:37 +0800

    :sparkles: support drag baseline

--- src/components/PreviewCard.vue
+++ src/components/PreviewCard.vue
@@ -1,92 +1,151 @@
 <template>
   <div id="cvsContainer">
-    <div id="innerContainer"
-          class="inline-flex"
-          :style="{
-            boxShadow: '0px 0px 10px #000'
-          }"
+    <div
+      id="innerContainer"
+      class="inline-flex"
+      :style="{
+        boxShadow: '0px 0px 10px #000',
+      }"
     >
-      <v-stage :config="previewStageConfig"  ref="stage" id="previewStage" @mousedown="handleStageMouseDown">
+      <v-stage
+        :config="previewStageConfig"
+        ref="stage"
+        id="previewStage"
+        @mousedown="handleStageMouseDown"
+      >
         <v-layer>
           <v-image :config="mainImgConfig"></v-image>
           <v-group :config="{}">
             <v-rect :config="bannerRectConfig"></v-rect>
-            <v-text :config="deviceInfoConfig" @dragEnd="dragDeviceInfoHook" @transformend="transformerDeviceInfoHook"></v-text>
-            <v-text :config="lensInfoConfig" @dragEnd="dragLensInfoHook" @transformend="transformerLensInfoHook"></v-text>
-            <v-image :config="iconInfoConfig" @dragEnd="dragIconInfoHook" @transformend="transformerIconInfoHook"></v-image>
-            <v-rect :config="verticalBarInfoConfig" @dragEnd="dragVerticalBarHook" @transformend="transformerVerticalBarHook"></v-rect>
-            <v-text :config="parameterInfoConfig" @dragEnd="dragParameterInfoHook" @transformend="transformerParameterInfoHook"></v-text>
-            <v-text :config="timeInfoConfig" @dragEnd="dragTimeInfoHook" @transformend="transformerTimeInfoHook"></v-text>
+            <v-text
+              :config="deviceInfoConfig"
+              @dragmove="dragMoveDeviceInfoHook"
+              @dragEnd="dragEndDeviceInfoHook"
+              @transformend="transformerDeviceInfoHook"
+              __useStrictMode
+            ></v-text>
+            <v-text
+              :config="lensInfoConfig"
+              @dragmove="dragMoveLensInfoHook"
+              @dragEnd="dragEndLensInfoHook"
+              @transformend="transformerLensInfoHook"
+              __useStrictMode
+            ></v-text>
+            <v-image
+              :config="iconInfoConfig"
+              @dragEnd="dragEndIconInfoHook"
+              @dragmove="dragMoveIconInfoHook"
+              @transformend="transformerIconInfoHook"
+              __useStrictMode
+            ></v-image>
+            <v-rect
+              :config="verticalBarInfoConfig"
+              @dragmove="dragMoveVerticalBarHook"
+              @dragEnd="dragEndVerticalBarHook"
+              @transformend="transformerVerticalBarHook"
+              __useStrictMode
+            ></v-rect>
+            <v-text
+              :config="parameterInfoConfig"
+              @dragmove="dragMoveParameterInfoHook"
+              @dragEnd="dragEndParameterInfoHook"
+              @transformend="transformerParameterInfoHook"
+              __useStrictMode
+            ></v-text>
+            <v-text
+              :config="timeInfoConfig"
+              @dragmove="dragMoveTimeInfoHook"
+              @dragEnd="dragEndTimeInfoHook"
+              @transformend="transformerTimeInfoHook"
+              __useStrictMode
+            ></v-text>
           </v-group>
           <v-transformer ref="transformer" :config="transformerConfig" />
+          <v-line :config="alignLineOneThirdConfig"></v-line>
+          <v-line :config="alignLineMiddleConfig"></v-line>
+          <v-line :config="alignLineTwoThirdConfig"></v-line>
         </v-layer>
       </v-stage>
     </div>
   </div>
   <div>
-    <el-pagination background layout="prev, pager, next" :total="30" @current-change="sizeChange" :disabled="parameterDisable" :current-page="themeIdx"/>
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="30"
+      @current-change="sizeChange"
+      :disabled="parameterDisable"
+      :current-page="themeIdx"
+    />
   </div>
 </template>
 
 <script setup>
-  import {ref} from 'vue'
-  import {defineRender} from "@/store/defineRender";
-  import {defineCanvasConfig} from "@/store/defineCanvasConfig";
-  import {themeIdx} from "@/store/defineThemes";
-  import {uid2Src} from "@/store/defineImg";
-  import {defineDragHooks} from "@/hooks/dragHooks";
-  import {defineTransformerHooks} from "@/hooks/transformerHooks";
-  const {currentRenderUid, parameterDisable, unMarshal}= defineRender()
+import { ref } from "vue";
+import { defineRender } from "@/store/defineRender";
+import { defineCanvasConfig } from "@/store/defineCanvasConfig";
+import { themeIdx } from "@/store/defineThemes";
+import { uid2Src } from "@/store/defineImg";
+import { defineDragHooks } from "@/hooks/dragHooks";
+import { defineTransformerHooks } from "@/hooks/transformerHooks";
+const { currentRenderUid, parameterDisable, unMarshal } = defineRender();
 
-  const {
-    dragVerticalBarHook,
-    dragLensInfoHook,
-    dragTimeInfoHook,
-    dragParameterInfoHook,
-    dragDeviceInfoHook,
-    dragIconInfoHook,
-  } = defineDragHooks()
+const {
+  dragEndVerticalBarHook,
+  dragMoveVerticalBarHook,
+  dragEndLensInfoHook,
+  dragMoveLensInfoHook,
+  dragEndTimeInfoHook,
+  dragMoveTimeInfoHook,
+  dragEndParameterInfoHook,
+  dragMoveParameterInfoHook,
+  dragEndDeviceInfoHook,
+  dragMoveDeviceInfoHook,
+  dragEndIconInfoHook,
+  dragMoveIconInfoHook,
+} = defineDragHooks();
 
-  const {
-    transformerVerticalBarHook,
-    transformerDeviceInfoHook,
-    transformerLensInfoHook,
-    transformerParameterInfoHook,
-    transformerTimeInfoHook,
-    transformerIconInfoHook,
-    handleStageMouseDown,
-    transformer
-  } = defineTransformerHooks()
+const {
+  transformerVerticalBarHook,
+  transformerDeviceInfoHook,
+  transformerLensInfoHook,
+  transformerParameterInfoHook,
+  transformerTimeInfoHook,
+  transformerIconInfoHook,
+  handleStageMouseDown,
+  transformer,
+} = defineTransformerHooks();
 
+const {
+  previewStageConfig,
+  mainImgConfig,
+  deviceInfoConfig,
+  parameterInfoConfig,
+  lensInfoConfig,
+  timeInfoConfig,
+  verticalBarInfoConfig,
+  iconInfoConfig,
+  bannerRectConfig,
+  transformerConfig,
+  alignLineMiddleConfig,
+  alignLineTwoThirdConfig,
+  alignLineOneThirdConfig,
+} = defineCanvasConfig();
 
-  const {
-    previewStageConfig,
-    mainImgConfig,
-    deviceInfoConfig,
-    parameterInfoConfig,
-    lensInfoConfig,
-    timeInfoConfig,
-    verticalBarInfoConfig,
-    iconInfoConfig,
-    bannerRectConfig,
-    transformerConfig,
-  } = defineCanvasConfig()
-
-  function sizeChange(e) {
-    const uid = currentRenderUid.value
-    if (!uid2Src.has(uid)) {
-      return
-    }
-    uid2Src.get(uid).renderThemeIdx = e
-    themeIdx.value = e
-
-    unMarshal(uid)
+function sizeChange(e) {
+  const uid = currentRenderUid.value;
+  if (!uid2Src.has(uid)) {
+    return;
   }
+  uid2Src.get(uid).renderThemeIdx = e;
+  themeIdx.value = e;
 
+  unMarshal(uid);
+}
 </script>
 
 <style>
-#cvsContainer{
+#cvsContainer {
   display: flex;
   width: 800px;
   flex-direction: row;
@@ -98,5 +157,4 @@
 #innerContainer {
   display: inline;
 }
-
 </style>
--- src/hooks/dragHooks.js
+++ src/hooks/dragHooks.js
@@ -1,74 +1,197 @@
-import {defineCanvasConfig} from "@/store/defineCanvasConfig";
-import {defineRender} from "@/store/defineRender";
-import {ref} from "vue";
-const {currentRenderUid,marshal} = defineRender()
+import { defineCanvasConfig } from "@/store/defineCanvasConfig";
+import { defineRender } from "@/store/defineRender";
 
+const { currentRenderUid, marshal, unMarshal } = defineRender();
 
 const {
-    previewStageConfig,
-    mainImgConfig,
-    deviceInfoConfig,
-    parameterInfoConfig,
-    lensInfoConfig,
-    timeInfoConfig,
-    verticalBarInfoConfig,
-    iconInfoConfig,
-    bannerRectConfig,
-} = defineCanvasConfig()
+  previewStageConfig,
+  mainImgConfig,
+  deviceInfoConfig,
+  parameterInfoConfig,
+  lensInfoConfig,
+  timeInfoConfig,
+  verticalBarInfoConfig,
+  iconInfoConfig,
+  bannerRectConfig,
+  alignLineMiddleConfig,
+  alignLineOneThirdConfig,
+  alignLineTwoThirdConfig,
+} = defineCanvasConfig();
 
-function dragVerticalBarHook(e) {
-    for (let key in verticalBarInfoConfig) {
-        verticalBarInfoConfig[key] = e.target.attrs[key]
-    }
-    marshal(currentRenderUid.value)
-}
-function dragDeviceInfoHook(e) {
-    for (let key in deviceInfoConfig) {
-        deviceInfoConfig[key] = e.target.attrs[key]
-    }
-    marshal(currentRenderUid.value)
-}
-
-function dragLensInfoHook(e) {
-    for (let key in lensInfoConfig) {
-        lensInfoConfig[key] = e.target.attrs[key]
-    }
-    marshal(currentRenderUid.value)
-
+function dragEndVerticalBarHook(e) {
+  for (let key in verticalBarInfoConfig) {
+    verticalBarInfoConfig[key] = e.target.attrs[key];
+  }
+  if (alignLineMiddleConfig.visible) {
+    const diff = verticalBarInfoConfig.height * 0.5;
+    verticalBarInfoConfig.y = alignLineMiddleConfig.points[1] - diff;
+    alignLineMiddleConfig.visible = false;
+  }
+  marshal(currentRenderUid.value);
+  alignLineMiddleConfig.visible = false;
 }
 
-function dragTimeInfoHook(e) {
-    for (let key in timeInfoConfig) {
-        timeInfoConfig[key] = e.target.attrs[key]
-    }
-    marshal(currentRenderUid.value)
+function dragMoveVerticalBarHook(evt) {
+  const x = evt.target.attrs.x;
+  const y = evt.target.attrs.y;
+  const centerX = verticalBarInfoConfig.width * 0.5 + x;
+  const centerY = verticalBarInfoConfig.height * 0.5 + y;
 
-}
-function dragParameterInfoHook(e) {
-    for (let key in parameterInfoConfig) {
-        parameterInfoConfig[key] = e.target.attrs[key]
-    }
-    marshal(currentRenderUid.value)
-}
-
-function dragIconInfoHook(e) {
-    for (let key in iconInfoConfig) {
-        iconInfoConfig[key] = e.target.attrs[key]
-    }
-    marshal(currentRenderUid.value)
+  const middle = alignLineMiddleConfig.points[1];
+  const oneThird = alignLineOneThirdConfig.points[1];
+  const twoThird = alignLineTwoThirdConfig[1];
+  const dist = 10;
+  alignLineMiddleConfig.visible = Math.abs(middle - centerY) < dist;
 }
 
+function dragEndDeviceInfoHook(e) {
+  for (let key in deviceInfoConfig) {
+    deviceInfoConfig[key] = e.target.attrs[key];
+  }
+  if (alignLineMiddleConfig.visible) {
+    deviceInfoConfig.y = alignLineMiddleConfig.points[1];
+    alignLineMiddleConfig.visible = false;
+  }
+  if (alignLineTwoThirdConfig.visible) {
+    deviceInfoConfig.y = alignLineTwoThirdConfig.points[1];
+    alignLineTwoThirdConfig.visible = false;
+  }
+  if (alignLineOneThirdConfig.visible) {
+    deviceInfoConfig.y = alignLineOneThirdConfig.points[1];
+    alignLineOneThirdConfig.visible = false;
+  }
+  marshal(currentRenderUid.value);
+}
 
+function dragMoveDeviceInfoHook(evt) {
+  const centerY = evt.target.attrs.y;
+  dragMoveText(centerY);
+}
+
+function dragEndLensInfoHook(e) {
+  for (let key in lensInfoConfig) {
+    lensInfoConfig[key] = e.target.attrs[key];
+  }
+  if (alignLineMiddleConfig.visible) {
+    lensInfoConfig.y = alignLineMiddleConfig.points[1];
+    alignLineMiddleConfig.visible = false;
+  }
+  if (alignLineTwoThirdConfig.visible) {
+    lensInfoConfig.y = alignLineTwoThirdConfig.points[1];
+    alignLineTwoThirdConfig.visible = false;
+  }
+  if (alignLineOneThirdConfig.visible) {
+    lensInfoConfig.y = alignLineOneThirdConfig.points[1];
+    alignLineOneThirdConfig.visible = false;
+  }
+  marshal(currentRenderUid.value);
+}
+
+function dragMoveLensInfoHook(evt) {
+  const centerY = evt.target.attrs.y;
+  dragMoveText(centerY);
+}
+
+function dragEndTimeInfoHook(e) {
+  for (let key in timeInfoConfig) {
+    timeInfoConfig[key] = e.target.attrs[key];
+  }
+  if (alignLineMiddleConfig.visible) {
+    timeInfoConfig.y = alignLineMiddleConfig.points[1];
+    alignLineMiddleConfig.visible = false;
+  }
+  if (alignLineTwoThirdConfig.visible) {
+    timeInfoConfig.y = alignLineTwoThirdConfig.points[1];
+    alignLineTwoThirdConfig.visible = false;
+  }
+  if (alignLineOneThirdConfig.visible) {
+    timeInfoConfig.y = alignLineOneThirdConfig.points[1];
+    alignLineOneThirdConfig.visible = false;
+  }
+  marshal(currentRenderUid.value);
+}
+
+function dragMoveTimeInfoHook(evt) {
+  const centerY = evt.target.attrs.y;
+  dragMoveText(centerY);
+}
+
+function dragEndParameterInfoHook(e) {
+  for (let key in parameterInfoConfig) {
+    parameterInfoConfig[key] = e.target.attrs[key];
+  }
+
+  if (alignLineMiddleConfig.visible) {
+    parameterInfoConfig.y = alignLineMiddleConfig.points[1];
+    alignLineMiddleConfig.visible = false;
+  }
+  if (alignLineTwoThirdConfig.visible) {
+    parameterInfoConfig.y = alignLineTwoThirdConfig.points[1];
+    alignLineTwoThirdConfig.visible = false;
+  }
+  if (alignLineOneThirdConfig.visible) {
+    parameterInfoConfig.y = alignLineOneThirdConfig.points[1];
+    alignLineOneThirdConfig.visible = false;
+  }
+  marshal(currentRenderUid.value);
+}
+
+function dragMoveParameterInfoHook(evt) {
+  const centerY = evt.target.attrs.y;
+  dragMoveText(centerY);
+}
+
+function dragEndIconInfoHook(e) {
+  for (let key in iconInfoConfig) {
+    iconInfoConfig[key] = e.target.attrs[key];
+  }
+  if (alignLineMiddleConfig.visible) {
+    const diff = iconInfoConfig.height * 0.5;
+    iconInfoConfig.y = alignLineMiddleConfig.points[1] - diff;
+    alignLineMiddleConfig.visible = false;
+  }
+
+  marshal(currentRenderUid.value);
+}
+
+function dragMoveIconInfoHook(evt) {
+  const x = evt.target.attrs.x;
+  const y = evt.target.attrs.y;
+  const centerX = iconInfoConfig.width * 0.5 + x;
+  const centerY = iconInfoConfig.height * 0.5 + y;
+
+  const middle = alignLineMiddleConfig.points[1];
+  const oneThird = alignLineOneThirdConfig.points[1];
+  const twoThird = alignLineTwoThirdConfig[1];
+  const dist = 20;
+  alignLineMiddleConfig.visible = Math.abs(middle - centerY) < dist;
+}
+
+function dragMoveText(centerY) {
+  const middle = alignLineMiddleConfig.points[1];
+  const oneThird = alignLineOneThirdConfig.points[1];
+  const twoThird = alignLineTwoThirdConfig.points[1];
+  const dist = 20;
+  alignLineMiddleConfig.visible = Math.abs(middle - centerY) < dist;
+  alignLineOneThirdConfig.visible = Math.abs(oneThird - centerY) < dist;
+  alignLineTwoThirdConfig.visible = Math.abs(twoThird - centerY) < dist;
+}
 
 function defineDragHooks() {
-    return {
-        dragTimeInfoHook,
-        dragDeviceInfoHook,
-        dragParameterInfoHook,
-        dragLensInfoHook,
-        dragVerticalBarHook,
-        dragIconInfoHook,
-    }
+  return {
+    dragEndTimeInfoHook,
+    dragMoveTimeInfoHook,
+    dragEndDeviceInfoHook,
+    dragMoveDeviceInfoHook,
+    dragEndParameterInfoHook,
+    dragMoveParameterInfoHook,
+    dragEndLensInfoHook,
+    dragMoveLensInfoHook,
+    dragEndVerticalBarHook,
+    dragMoveVerticalBarHook,
+    dragEndIconInfoHook,
+    dragMoveIconInfoHook,
+  };
 }
 
-export {defineDragHooks}
+export { defineDragHooks };
