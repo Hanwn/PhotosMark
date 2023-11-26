82a4f1c48bf291a3a527127b73f9c5dd2e43f83b
hanwn <hanwn7721@gmail.com>
2023-11-26 12:16:08 +0800

    :sparkles: introduce axios

--- src/hooks/defineOnMountedHook.js
+++ src/hooks/defineOnMountedHook.js
@@ -1,15 +1,16 @@
 import axios from "axios";
-import { setSettings } from "@/store/defineSettings";
 import { ref } from "vue";
-import { ElMessage } from "element-plus";
 import { requestNotifyStatus } from "@/utils/notify";
+import { request } from "@/apis/request";
+import { setSettings } from "@/store/defineSettings";
 
 function useAppOnMountedHook() {
   const fetchSettings = async () => {
-    let resp;
     try {
-      resp = await axios.get("http://localhost:8080/settings");
-      resp.status === 200 ? setSettings(resp.data) : "";
+      // resp = await axios.get("http://localhost:8080/settings");
+      const data = await request.get("api/settings");
+      if (data === null) return;
+      setSettings(data);
     } catch (e) {
       requestNotifyStatus("Oops, this is a error message.", "error");
     }
@@ -24,22 +25,23 @@ function useParameterAreaOnMountedHook() {
   const options = ref([]);
   const fetchIconList = async () => {
     try {
-      let resp = await axios.get("http://localhost:8080/iconList");
-      if (resp.status === 200) {
-        const data = resp.data;
-        for (let key in data) {
-          const item = {
+      // let resp = await axios.get("http://localhost:8080/api/iconList");
+      const data = await request.get("api/iconList");
+      if (data === null) {
+        return;
+      }
+      for (let key in data) {
+        const item = {
+          label: key,
+          options: [],
+        };
+        for (let i = 0; i < data[key].length; i++) {
+          item.options.push({
             label: key,
-            options: [],
-          };
-          for (let i = 0; i < data[key].length; i++) {
-            item.options.push({
-              label: key,
-              value: data[key][i],
-            });
-          }
-          options.value.push(item);
+            value: data[key][i],
+          });
         }
+        options.value.push(item);
       }
     } catch (e) {
       // ElMessage.error("Oops, this is a error message.");
